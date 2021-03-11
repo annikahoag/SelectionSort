3806bd7bb0665501f443e43634a2b15fa8ea5833
annika_hoag <annika_hoag@10.0.0.143>
2021-03-11 13:50:41 -0500

    1. fixed bug in Main

--- SelectionSort/src/Main.java
+++ SelectionSort/src/Main.java
@@ -12,52 +12,55 @@ public class Main {
 		all.addStudent();
 		
 		while (runAgain) {
-			
-			//ask user what they want to do 
-			System.out.println("What would you like to do? Please enter: "
-					+ "\n1 to print all students by name. "
-					+ "\n2 to print all students by final average."
-					+ "\n3 to add a student. "
-					+ "\n4 to print mean, median, mode, and standard deviation of quarterly averages. "
-					+ "\n5 to stop program.");
-			userInput = scn.nextInt();
-			
-			switch(userInput) {
+			try {
+				//ask user what they want to do 
+				System.out.println("What would you like to do? Please enter: "
+						+ "\n1 to print all students by name. "
+						+ "\n2 to print all students by final average."
+						+ "\n3 to add a student. "
+						+ "\n4 to print mean, median, mode, and standard deviation of quarterly averages. "
+						+ "\n5 to stop program.");
+				userInput = scn.nextInt();
 				
-				//print by name
-				case 1: userInput=1;
-					all.sortByName();
-					runAgain=true;
-					break;
-				
-				//print by final average	
-				case 2: userInput=2;
-					all.sortByAverage();
-					runAgain=true;
-					break;
-				
-				//add students	
-				case 3: userInput=3;
-					all.addStudent();
-					runAgain=true;
-					break;
-				
-				//EXTRA CREDIT: mean, median, mode and standard deviation of quarter averages
-				case 4: userInput=4;
-					all.sortArray();
-					all.mean();
-					all.median();
-					all.mode();
-					all.stanDev();
-					runAgain=true;
-					break;
-				
-				//end program	
-				case 5: userInput=5;
-					runAgain=false;
-					break;
+				switch(userInput) {
+					
+					//print by name
+					case 1: userInput=1;
+						all.sortByName();
+						runAgain=true;
+						break;
+					
+					//print by final average	
+					case 2: userInput=2;
+						all.sortByAverage();
+						runAgain=true;
+						break;
+					
+					//add students	
+					case 3: userInput=3;
+						all.addStudent();
+						runAgain=true;
+						break;
+					
+					//EXTRA CREDIT: mean, median, mode and standard deviation of quarter averages
+					case 4: userInput=4;
+						all.sortArray();
+						all.mean();
+						all.median();
+						all.mode();
+						all.stanDev();
+						runAgain=true;
+						break;
+					
+					//end program	
+					case 5: userInput=5;
+						runAgain=false;
+						break;
+				}
+			}catch(java.util.InputMismatchException e) {
+				System.out.println("Please only enter numbers. Program will end.");
+				runAgain=false;
 			}
-			
 		}//end of while
 		
 		scn.close();
